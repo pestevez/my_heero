73200bb6e1afe13193458c5b2ecf7b04568ec14a
Pablo Estevez <pestevez@fb.com>
2020-12-30 11:01:54 -0500

    Add logging

--- __init__.py
+++ __init__.py
@@ -1,4 +1,11 @@
+"""Custom Eeero Integration"""
+import logging
+
 DOMAIN = "my_heero"
 
+_LOGGER = logging.getLogger(__name__)
+
+
 async def async_setup(hass, config):
+  _LOGGER.info("Setting up My Heero integration...")
   return True
