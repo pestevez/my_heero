30876c4d853c855749ac6b08b4220159b00e0a06
Pablo Estevez <pestevez@fb.com>
2020-12-30 22:38:31 -0500

    Set default domain

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ from .const import (
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass, config):
+  hass.data.setdefault(DOMAIN, {})
   return True
 
 #async def async_setup_entry(hass, self):
